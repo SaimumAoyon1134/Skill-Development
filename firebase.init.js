dee5b0b402a204e0bad85e3a369da5e3597bf497
SaimumAoyon1134 <saimum681@gmail.com>
2025-10-22 00:03:25 +0600

    third Commit

--- firebase.init.js
+++ firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBljNYdDqLFYaX9oIIAPHnEEI8-YcS1xG4",
-  authDomain: "my-application-40e3b.firebaseapp.com",
-  projectId: "my-application-40e3b",
-  storageBucket: "my-application-40e3b.firebasestorage.app",
-  messagingSenderId: "652993151581",
-  appId: "1:652993151581:web:7315428f3c2a2dba5283b4",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
